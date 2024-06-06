c7d902b8fb1ccd6c0b41a0dbc81d7e4a52d40a13
seaspore <xagargm2@gmail.com>
2024-06-06 20:11:04 +0200

    muestrario

--- test/script.js
+++ test/script.js
@@ -13,33 +13,20 @@ const onProgress = (event) => {
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
 
 // Handles the slider
-const modelViewer = document.querySelector("model-viewer");
+const modelViewer = document.querySelector('model-viewer');
 
-  window.switchSrc = (element, name) => {
-    const base = "./assets/" + name;
-    modelViewer.src = base + '.glb';
-    modelViewer.poster = base + '.webp';
-    const slides = document.querySelectorAll(".slide");
-    slides.forEach((element) => {element.classList.remove("selected");});
-    element.classList.add("selected");
-  };
-
-  document.querySelector(".slider").addEventListener('beforexrselect', (ev) => {
-    // Keep slider interactions from affecting the XR scene.
-    ev.preventDefault();
+window.switchSrc = (element, name) => {
+  const base = './assets/' + name;
+  modelViewer.src = base + '.glb';
+  modelViewer.poster = base + '.webp';
+  const slides = document.querySelectorAll('.slide');
+  slides.forEach((element) => {
+    element.classList.remove('selected');
   });
+  element.classList.add('selected');
+};
 
-// Handles model variants
-const modelViewerVariants = document.querySelector("model-viewer");
-
-function handleVariant(data) {
-  var variant = data.getAttribute("variant");
-  modelViewerVariants.variantName = variant === 'default' ? null : variant;
-  const slides = document.querySelectorAll(".slide");
-  slides.forEach((element) => {element.classList.remove("selected");});
-  data.classList.add("selected");
-}
-/*
-select.addEventListener('input', (event) => {
-  modelViewerVariants.variantName = event.target.value === 'default' ? null : event.target.value;
-});*/
+document.querySelector('.slider').addEventListener('beforexrselect', (ev) => {
+  // Keep slider interactions from affecting the XR scene.
+  ev.preventDefault();
+});
