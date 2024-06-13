08243d0cb475875fd089435a665ff4c524f0b38b
seaspore <xagargm2@gmail.com>
2024-06-13 02:28:21 +0200

    new experience

---
+++ script.js
@@ -0,0 +1,34 @@
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+    event.target.removeEventListener('progress', onProgress);
+  } else {
+    progressBar.classList.remove('hide');
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+// Handles the slider
+const modelViewer = document.querySelector('model-viewer');
+const button = document.querySelector('#vr-button');
+
+window.switchSrc = (element, name) => {
+  const base = './assets/' + name;
+  modelViewer.src = base + '.glb';
+  modelViewer.poster = base + '.webp';
+  const slides = document.querySelectorAll('.slide');
+  slides.forEach((element) => {
+    element.classList.remove('selected');
+  });
+  element.classList.add('selected');
+  button.href = 'https://seaspore.github.io/seaspore/' + name;
+};
+
+document.querySelector('.slider').addEventListener('beforexrselect', (ev) => {
+  // Keep slider interactions from affecting the XR scene.
+  ev.preventDefault();
+});
